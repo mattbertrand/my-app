39ff2e18ae6fb516d409bcbfbc8e48b317f52763
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-06-05 15:53:56 -0700

    iterate through each object

--- src/App.js
+++ src/App.js
@@ -3,9 +3,44 @@ import './App.css'
 import ContactCard from './ContactCard'
 
 const App = () => {
+  const contacts = [
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+    name: "Matthieu Bertrand",
+    email: "matt@matthieubertrand.com",
+    age: 32
+  },
+  {
+    avatarUrl: "https://via.placeholder.com/150",
+    name: "Paul Jones",
+    email: "pj@example.com" ,
+    age: 25
+  },
+  {
+    avatarUrl: "https://via.placeholder.com/150",
+    name: "Peter Pan" ,
+    email: "pp@mpeterpan.com" ,
+    age: 100
+  },
+  {
+    avatarUrl: "https://via.placeholder.com/150",
+    name: "Test test",
+    email: "test@test.com" ,
+    age: 150
+  }
+  ]
   return (
     <div>
-      <ContactCard 
+      { contacts.map((contact, index) => {
+        return (
+          <ContactCard key={index}
+      avatarUrl = { contact.avatarUrl } 
+      name = { contact.name }
+      email= { contact.email }
+      age= { contact.age }/>
+        )
+      }) }
+      {/* <ContactCard 
       avatarUrl = "https://via.placeholder.com/150" 
       name ="Matthieu Bertrand" 
       email="matt@matthieubertrand.com" 
@@ -19,7 +54,7 @@ const App = () => {
       avatarUrl = "https://via.placeholder.com/150" 
       name ="Peter Pan" 
       email="pp@mpeterpan.com" 
-      age= {100}/>
+      age= {100}/> */}
     </div>
   )
 }
