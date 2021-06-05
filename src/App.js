e553eaf537eb14d15b8579368f2914c6f08673af
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-06-05 15:42:31 -0700

    used props to update component dynimacally

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import ContactCard from './ContactCard'
 const App = () => {
   return (
     <div>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      <ContactCard 
+      avatarUrl = "https://via.placeholder.com/150" 
+      name ="Matthieu Bertrand" 
+      email="matt@matthieubertrand.com" 
+      age= {32}/>
+      <ContactCard 
+      avatarUrl = "https://via.placeholder.com/150" 
+      name ="Paul Jones" 
+      email="pj@example.com" 
+      age= {25}/>
+      <ContactCard 
+      avatarUrl = "https://via.placeholder.com/150" 
+      name ="Peter Pan" 
+      email="pp@mpeterpan.com" 
+      age= {100}/>
     </div>
   )
 }
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 
-const ContactCard = () => {
+const ContactCard = (props) => {
     const [showAge, setShowAge] = useState(false)
     return (
         <div className="contact-card">
-            <img alt="profile" src="https://via.placeholder.com/150"></img>
+            <img alt="profile" src={ props.avatarUrl }></img>
             <div className="user-details">
-                <p>Name: Matthieu Bertrand</p>
-                <p>Email: matt@matthieubertrand.com</p>
+                <p>{ props.name }</p>
+                <p>{ props.email }</p>
                 <button onClick={() => setShowAge(!showAge)}>Show Age</button>
-                { showAge && <p>Age: 32</p> }
+                { showAge && <p>Age: { props.age}</p> }
             </div>
         </div>
     )
