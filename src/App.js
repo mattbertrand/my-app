5cac15bb920955efc3eb9d7a74294269936acd6c
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-06-05 16:09:58 -0700

    loaded the data from backen api

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import ContactCard from './ContactCard'
 
 const App = () => {
-  const contacts = [
-    {
-      avatarUrl: "https://via.placeholder.com/150",
-    name: "Matthieu Bertrand",
-    email: "matt@matthieubertrand.com",
-    age: 32
-  },
-  {
-    avatarUrl: "https://via.placeholder.com/150",
-    name: "Paul Jones",
-    email: "pj@example.com" ,
-    age: 25
-  },
-  {
-    avatarUrl: "https://via.placeholder.com/150",
-    name: "Peter Pan" ,
-    email: "pp@mpeterpan.com" ,
-    age: 100
-  },
-  {
-    avatarUrl: "https://via.placeholder.com/150",
-    name: "Test test",
-    email: "test@test.com" ,
-    age: 150
-  }
-  ]
+  
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+    .then(response => response.json())
+    .then(data => {
+      setResults(data.results)
+    })
+  }, [])
+
+  
   return (
     <div>
-      { contacts.map((contact, index) => {
+      { results.map((result, index) => {
         return (
           <ContactCard key={index}
-      avatarUrl = { contact.avatarUrl } 
-      name = { contact.name }
-      email= { contact.email }
-      age= { contact.age }/>
+      avatarUrl = { result.picture.large } 
+      name = { result.name.first }
+      email= { result.email }
+      age= { result.dob.age }/>
         )
       }) }
-      {/* <ContactCard 
-      avatarUrl = "https://via.placeholder.com/150" 
-      name ="Matthieu Bertrand" 
-      email="matt@matthieubertrand.com" 
-      age= {32}/>
-      <ContactCard 
-      avatarUrl = "https://via.placeholder.com/150" 
-      name ="Paul Jones" 
-      email="pj@example.com" 
-      age= {25}/>
-      <ContactCard 
-      avatarUrl = "https://via.placeholder.com/150" 
-      name ="Peter Pan" 
-      email="pp@mpeterpan.com" 
-      age= {100}/> */}
     </div>
   )
 }
